6530eae238e7b5ac7932b96f92e80cec7d8733f3
Pietro Napoleão <60191373+pietronapoleao@users.noreply.github.com>
2021-11-09 21:35:40 -0300

    HiApp
    teste

---
+++ src/screens/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+
+const Profile = () => {
+    return(
+        <View style={{
+            flex:1,
+            alignItems:'center',
+            justifyContent:'center'
+        }}>
+            <Text>Perfil</Text>
+        </View>
+    )
+}
+export default Profile;
